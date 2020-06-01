a4757d8c43e22b3974913dd19657077a8feecbb9
CJ Harries <cj@wizardsoftheweb.pro>
2020-06-01 14:25:12 -0500

    Draft single spreadsheet load

--- R/process_requests.R
+++ R/process_requests.R
@@ -31,3 +31,22 @@ parse_spreadsheet_urls <- function(data_html = scrape_source_page()) {
 
   urls_to_scrape
 }
+
+get_etl_parser <- function(etl_parser_name) {
+  full_name <- paste0("etl_parser_", etl_parser_name)
+  if (exists(full_name, envir = as.environment("package:txronaparser"))) {
+    return(as.environment("package:txronaparser"))
+  }
+  function(x) { list() }
+}
+
+load_single_spreadsheet_data <- function(spreadsheet_name) {
+  spreadsheet_config <- trp_config$spreadsheets[[spreadsheet_name]]
+  new_raw_data <- load_remote_spreadsheet(spreadsheet_config$url)
+  new_parsed_data <- get_etl_parser(spreadsheet_config$etl_parser)(new_raw_data)
+
+  list(
+    "raw" = new_raw_data,
+    "parsed" = new_parsed_data
+  )
+}
