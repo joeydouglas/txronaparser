f61db1acb8be5097f301b3f8e2423356c5945eb3
CJ Harries <cj@wizardsoftheweb.pro>
2020-06-01 13:17:23 -0500

    Define config schema

--- R/constants.R
+++ R/constants.R
@@ -1,1 +1,2 @@
 DEFAULT_CONFIG_FILE_PATH <- system.file("extdata", "default-config.yml", package="txronaparser")
+CONFIG_SCHEMA_PATH <- system.file("extdata", "config.schema.json", package="txronaparser")
