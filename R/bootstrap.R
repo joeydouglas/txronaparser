4c3fcc3eebad22ce22060d52bb2593f1c54a79c8
CJ Harries <cj@wizardsoftheweb.pro>
2020-06-01 13:23:54 -0500

    Update bootstrap calls

--- R/bootstrap.R
+++ R/bootstrap.R
@@ -1,20 +1,13 @@
 #' @export
-bootstrap <- function() {
-  dir.create(
-    TRP_CONFIG("working_directory"),
-    showWarnings = FALSE,
-    recursive = TRUE
-  )
-  for (data_prefix in names(TRP_CONFIG("data_lists"))) {
-    if (TRP_CONFIG("data_lists")[[data_prefix]]$save) {
-      dir.create(
-        file.path(
-        TRP_CONFIG("working_directory"),
+bootstrap <- function(config_file_path = NULL) {
+  load_config(config_file_path = config_file_path)
+  write_config(config)
+  for (data_prefix in names(config$data_lists)) {
+    if (config$data_lists[[data_prefix]]$save) {
+      fs::dir_create(fs::path_join(
+        config$working_directory,
         data_prefix
-        ),
-        showWarnings = FALSE,
-        recursive = TRUE
-      )
+      ))
     }
   }
 }
@@ -22,7 +15,7 @@ bootstrap <- function() {
 #' @export
 nuke_content <- function(are_you_sure = FALSE) {
   if (are_you_sure) {
-    unlink(TRP_CONFIG("working_directory"), recursive = TRUE)
+    fs::file_delete(config$working_directory)
   } else {
     warning(WARNING_NUKE_CONTENT)
   }
