bc76e4dcd88f4da947a38d023a2ac52c140a68f9
CJ Harries <cj@wizardsoftheweb.pro>
2020-06-01 13:58:21 -0500

    Split out requesting vs parsing

--- R/make_requests.R
+++ R/make_requests.R
@@ -25,36 +25,3 @@ scrape_source_page <- function(url_to_scrape = trp_config$dshs_data_source$url, 
 
   data_html
 }
-
-# Finds all the Texas spreadsheets on the page
-#' @importFrom magrittr %>%
-parse_spreadsheet_urls <- function(data_html = scrape_source_page()) {
-  urls_to_scrape <- data_html %>%
-    xml2::xml_find_all(paste0(
-      paste0("//h2[./u[text() = '", trp_config$dshs_data_source$h2_lead, "']]"),
-      "/following-sibling::h6",
-      "/a[",
-      paste0("starts-with(@href, '", trp_config$dshs_data_source$href_lead, "')"),
-      " and ",
-      "(",
-      "substring(@href, string-length(./@href) - 3) = 'xlsx'",
-      " or ",
-      "substring(@href, string-length(./@href) - 2) = 'xls'",
-      ")",
-      "]"
-    )
-    ) %>%
-    xml2::xml_attr("href") %>%
-    sapply(
-      .,
-      function(input) {
-        paste0(
-          gsub("/corona.*$", "", trp_config$dshs_data_source$url),
-          input
-        )
-      },
-      USE.NAMES = FALSE
-    )
-
-  urls_to_scrape
-}
