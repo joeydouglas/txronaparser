cbfdb823ed8f649a2ad4b50d542e606f51a5cdec
CJ Harries <cj@wizardsoftheweb.pro>
2020-06-01 16:46:50 -0500

    Document exported local data io fncs

--- R/local-data-io.R
+++ R/local-data-io.R
@@ -1,3 +1,8 @@
+#' Saves a single data object
+#'
+#' @param data_name The object name
+#' @param data_contents The actual value
+#' @param data_list_type The data list type to store it with
 #' @export
 save_single_data_local <- function(data_name, data_contents, data_list_type = "misc") {
   data_directory <- fs::path(trp_config$working_directory, data_list_type)
@@ -11,6 +16,10 @@ save_single_data_local <- function(data_name, data_contents, data_list_type = "m
   )
 }
 
+#' Saves a specific data list
+#'
+#' @param data_list The list to walk over and store
+#' @param data_list_type The data list type to store it with
 #' @export
 save_data_list_local <- function(data_list, data_list_type = "misc") {
   for (data_name in names(data_list)) {
@@ -18,6 +27,9 @@ save_data_list_local <- function(data_list, data_list_type = "misc") {
   }
 }
 
+#' Saves all data present in the singleton \code{all_data}
+#'
+#' @param new_data The data to store; default is \code{all_data}
 #' @export
 save_all_data_local <- function(new_data = all_data) {
   for (data_list_type in names(new_data)) {
@@ -25,6 +37,12 @@ save_all_data_local <- function(new_data = all_data) {
   }
 }
 
+#' Loads a single data object from either its name and list type or a full path
+#'
+#' @param data_name The object name
+#' @param data_list_type The data list type to load
+#' @param data_full_path Default \code{NULL}; if set will be used as the RDS path
+#' @return The contents of the loaded RDS file
 #' @export
 load_single_data_local <- function(data_name, data_list_type = "misc", data_file_path = NULL) {
   if (is.null(data_file_path)) {
@@ -40,6 +58,10 @@ load_single_data_local <- function(data_name, data_list_type = "misc", data_file
   readRDS(data_path)
 }
 
+#' Loads an entire data list type
+#'
+#' @param data_list_type The data list type to load
+#' @return The entire loaded list
 #' @export
 load_data_list_local <- function(data_list_type = "misc") {
   data_files <- fs::dir_ls(
@@ -58,6 +80,8 @@ load_data_list_local <- function(data_list_type = "misc") {
   loaded_data
 }
 
+#' Loads all data on disk into the singleton \code{.GlobalEnv$all_data}
+#'
 #' @export
 load_all_data_local <- function() {
   loaded_data <- list()
