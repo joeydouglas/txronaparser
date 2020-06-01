c24d256225bef093e8adbe6436be7bb87cff9603
CJ Harries <cj@wizardsoftheweb.pro>
2020-06-01 10:49:48 -0500

    Add bootstrap function

--- R/bootstrap.R
+++ R/bootstrap.R
@@ -1,15 +1,15 @@
 #' @export
 bootstrap <- function() {
   dir.create(
-    TRP_CONFIG$working_directory,
+    TRP_CONFIG("working_directory"),
     showWarnings = FALSE,
     recursive = TRUE
   )
-  for (data_prefix in names(TRP_CONFIG$save_data)) {
-    if (TRP_CONFIG$data_lists[[data_prefix]]$save) {
+  for (data_prefix in names(TRP_CONFIG("data_lists"))) {
+    if (TRP_CONFIG("data_lists")[[data_prefix]]$save) {
       dir.create(
         file.path(
-        TRP_CONFIG$working_directory,
+        TRP_CONFIG("working_directory"),
         data_prefix
         ),
         showWarnings = FALSE,
